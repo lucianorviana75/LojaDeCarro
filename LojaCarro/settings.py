3a5f85eb7ad35ac10673919e6d4f53760e35adc9
luciano ribeiro viana <ribeirovianaluciano98@gmail.com>
2024-03-27 15:58:45 -0300

    mais dados

--- LojaCarro/settings.py
+++ LojaCarro/settings.py
@@ -15,6 +15,7 @@ import django_heroku
 import os
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,3 +129,4 @@ django_heroku.settings(locals())
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
